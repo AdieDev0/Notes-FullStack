e7333db3dbb7fc8ec590e861b7571c3705f10f38
AdieDev0 <devutella@gmail.com>
2024-12-02 10:45:01 +0800

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
-import SignUp from './pages/SignUp/SignUp'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-      <Login />
-      <SignUp/>
-      
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/dashboard" exact element={<Home />}></Route>
+        <Route path="/login" exact element={<Login />}></Route>
+        <Route path="/singUp" exact element={<SignUp />}></Route>
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
